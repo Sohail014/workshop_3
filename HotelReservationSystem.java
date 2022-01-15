724e596bd4ab638fb7cd1102b8e06cf5fb1fc50c
Mohd Sohail <93813531+Sohail014@users.noreply.github.com>
2022-01-15 09:25:33 +0530

    Delete HotelReservationSystem.java

--- HotelReservationSystem.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelab.workshop3;
-
-public class HotelReservationSystem {
-    public static void main(String[] args) {
-            System.out.println(" Welcome to Hotel Reservation Program ");
-        }
-}
