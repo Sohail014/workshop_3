860f7f9eeda80b0e2d791621b38685ae14975f70
Mohd Sohail <93813531+Sohail014@users.noreply.github.com>
2022-01-15 16:02:47 +0530

    Added  Ability to find cheapest hotel

---
+++ HotelReservationSystem.java
@@ -0,0 +1,19 @@
+package com.bridgelab.workshop3;
+
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelSetup hotelSetup=new HotelSetup();
+        hotelSetup.addHotel();
+        hotelSetup.showHotelInfo();
+
+
+        System.out.println(" ");
+        System.out.println("*Cheapest Hotel Based on WeekdayRegularRate");
+        hotelSetup.findCheapestHotelOne("01-Jan-2021","05-Jan-2021");
+        System.out.println(" ");
+
+
+    }
+}}
